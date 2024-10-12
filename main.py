f1489c6013d118db7cf093567fc5ce7bac593490
Артём Роот <root-artem@mail.ru>
2024-10-12 16:00:04 +0700

    start practice

---
+++ main.py
@@ -0,0 +1,54 @@
+from typing import List
+import uvicorn
+
+from fastapi import FastAPI
+
+from models.shemas import Trade, User
+
+
+app = FastAPI(
+    title='Trading App'
+)
+
+
+fake_users = [
+    {'id': 1, 'role': 'admin', 'name': 'Bob'},
+    {'id': 2, 'role': 'investor', 'name': 'Jhon'},
+    {'id': 3, 'role': 'trader', 'name': 'Matt'},
+    {'id': 4, 'role': 'investor', 'name': 'Homer', 'degree': [
+        {'id': 1, 'created_at': '2020-01-01T00:00:00', 'type_degree': 'expert'}
+    ]}
+]
+
+
+fake_trades = [
+    {'id': 1, 'user_id': 1, 'currency': 'BTC', 'side': 'buy', 'price': 123, 'amount': 2.12},
+    {'id': 2, 'user_id': 1, 'currency': 'BTC', 'side': 'sell', 'price': 125, 'amount': 2.12}
+]
+
+
+@app.get('/users/{user_id}', response_model=List[User])
+def get_user(user_id: int):
+    return [user for user in fake_users if user.get('id') == user_id]
+
+
+@app.post('/users/{user_id}')
+def change_user_name(user_id: int, new_name: str):
+    current_user = list(filter(lambda user: user.get('id') == user_id, fake_users))[0]
+    current_user['name'] = new_name
+    return {'status': 200, 'data': current_user}
+
+
+@app.get('/trades')
+def get_trades(limit: int = 1, offset: int = 0):
+    return fake_trades[offset:][:limit]
+
+
+@app.post('/trades')
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {'status': 200, 'data': fake_trades}
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)
