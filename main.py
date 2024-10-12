c2373e9cf645f1c73ea3b23158a70bda49adfdca
Артём Роот <root-artem@mail.ru>
2024-10-12 20:02:18 +0700

    add auth

--- main.py
+++ main.py
@@ -1,53 +1,45 @@
 from typing import List
+import fastapi_users
 import uvicorn
 
+from fastapi import Depends, FastAPI
 from fastapi import FastAPI
+from fastapi_users import FastAPIUsers
 
-from models.shemas import Trade, User
+from auth.auth import auth_backend
+from auth.manager import get_user_manager
+from auth.schemas import UserCreate, UserRead
+from auth.database import User
+
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
 
 
 app = FastAPI(
     title='Trading App'
 )
 
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
 
-fake_users = [
-    {'id': 1, 'role': 'admin', 'name': 'Bob'},
-    {'id': 2, 'role': 'investor', 'name': 'Jhon'},
-    {'id': 3, 'role': 'trader', 'name': 'Matt'},
-    {'id': 4, 'role': 'investor', 'name': 'Homer', 'degree': [
-        {'id': 1, 'created_at': '2020-01-01T00:00:00', 'type_degree': 'expert'}
-    ]}
-]
-
-
-fake_trades = [
-    {'id': 1, 'user_id': 1, 'currency': 'BTC', 'side': 'buy', 'price': 123, 'amount': 2.12},
-    {'id': 2, 'user_id': 1, 'currency': 'BTC', 'side': 'sell', 'price': 125, 'amount': 2.12}
-]
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
 
 
-@app.get('/users/{user_id}', response_model=List[User])
-def get_user(user_id: int):
-    return [user for user in fake_users if user.get('id') == user_id]
+current_user = fastapi_users.current_user()
 
-
-@app.post('/users/{user_id}')
-def change_user_name(user_id: int, new_name: str):
-    current_user = list(filter(lambda user: user.get('id') == user_id, fake_users))[0]
-    current_user['name'] = new_name
-    return {'status': 200, 'data': current_user}
-
-
-@app.get('/trades')
-def get_trades(limit: int = 1, offset: int = 0):
-    return fake_trades[offset:][:limit]
-
-
-@app.post('/trades')
-def add_trades(trades: List[Trade]):
-    fake_trades.extend(trades)
-    return {'status': 200, 'data': fake_trades}
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.email}"
 
 
 if __name__ == '__main__':
